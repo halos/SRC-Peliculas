88ca30868c9f8fbab9a41c1079bfc29541cb6194
sramirez <srg00017@gmail.com>
2010-12-11 12:45:30 +0100

    Actualización

--- estrategiaPrediccion.py
+++ estrategiaPrediccion.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+__author__="sramirez"
+__date__ ="$07-dic-2010 10:25:53$"
 
-class EstrategisPrediccion:
+
+class EstrategiaPrediccion:
 	""" Interfaz de la estrategia de predicción """
 	
 	def __init__(self,  pred_func):
@@ -12,5 +15,48 @@ class EstrategisPrediccion:
 	
 			pred_func(): Función de predicción
 		"""
-		
+
 		self.predice = pred_func
+
+	def predice(self, idItem, lvaloraciones, lsimilitudes):
+		"""
+			
+		Metodo que devuelve el valor de prediccion para un item-usuario
+		
+		Params:
+		
+				idItem: Identificador del item cuyo valora deseamos predecir
+				lvaloraciones(list): Contiene todos las valoraciones aportadas por todos los usuarios
+				lsimilitudes(list): Contiene todos los pares de similitud entre items del sistema			
+
+		Return:
+					
+				prediccion(Valoracion): Valoración predicha para un valor desconocido
+					
+		"""
+			
+		dvaloraciones = {}
+			
+		for valoracion in lvaloraciones:
+			idUsu = valoracion.getUsu()
+			idItem = valoracion.getItem()
+			if idUsu not in dvaloraciones:
+				dvaloraciones[idUsu] = {}
+			dvaloraciones[idUsu, idItem] = valoracion.getVal()
+		
+		dsimilitudes = {}
+			
+		for similitud in lsimilitudes:
+			idItem1 = similitud.getUsu()
+			idItem2 = similitud.getItem()
+			if idItem1 not in dvaloraciones:
+				dsimilitudes[idItem1] = {}
+			dsimilitudes[idItem1, idItem2] = similitud.getVal()
+		
+		prediccion = self.predice(self, idItem, dvaloraciones, dsimilitudes)
+				
+		return prediccion
+			
+		
+			
+            
