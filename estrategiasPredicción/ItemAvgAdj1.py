02b1dc7af373e1bbff4e7b5ea6e100c8b585944a
Sergio Ramírez <srg00017@gmail.com>
2011-08-04 19:16:49 +0200

    Reestructuración completa

--- estrategiasPredicción/ItemAvgAdj1.py
+++ estrategiasPredicción/ItemAvgAdj1.py
@@ -4,74 +4,77 @@
 __author__="sramirez"
 __date__ ="$07-dic-2010 10:22:31$"
 
-import sys
-sys.path.append("..")
-
 from math import fabs
 from valoracion import Valoracion
 
 class ItemAvgAdj1:
-	""" Clase que implementa el método de prediccion Item Average Adjustament (All-1) """
+	""" Clase que implementa el método de prediccion
+		Item Average Adjustament (All-1) 
+		¡No se tiene en cuenta la valoración del item a predecir!	
+	"""
 	
-	def __init__(self):
+	def __init__(self, motor):
 		""" Constructor básico"""
+		self.__motor = motor
 
 
-	def mediausuario(self, idUsu, valoraciones):
+	def __mediausuario(self, idUsu, idItem):
 		"""
-			Metodo que calcula la media de las valoraciones hechas por un usuario a todos sus items
+			Metodo que calcula la media de las valoraciones
+			hechas por un usuario a todos sus items
 			
 			Params:
-					
-					valoraciones(dict): Valoraciones hechas por todos los usuarios para todos los items
+					idUsu (Integer):
+					idItem (Integer):
 					
 			Return:
-					media_usuario: Media de las valoraciones hechas por un usuario a todos sus items
+					media_usuario (Float): Media de las valoraciones hechas por un usuario a todos sus items
 			
 		"""
-			
-		lval_usuario = valoraciones.get(idUsu)
+		
+		lval_usuario = self.__motor.getValoracionesUsuario(idUsu)
 		nval = 0
 		media_usuario = 0
 		for valoracion in lval_usuario:
-			media_usuario += valoracion.valoracion
-			nval+= 1
-		media_usuario = media_usuario / nval
+			if valoracion.idPel != idItem: #Obviamos la valoracion del item a predecir
+				media_usuario += valoracion.valoracion
+				nval+= 1
+		media_usuario /= nval
 		return media_usuario
 
-	def mediaitem(self, idItem, valoraciones):
+	def __mediaitem(self, idUsu, idItem):
 
 		"""
-			Metodo que calcula la media de las valoraciones hechas para un determinado item
+			Metodo que calcula la media de las valoraciones
+			hechas para un determinado item
+			
 			Params:
-					
-					valoraciones(dict): Valoraciones hechas por todos los usuarios para todos los items
+					idUsu	(Integer):
+					idItem	(Integer): 
 					
 			Return:
 					media_usuario: Media de las valoraciones hechas para un determinado item
 			
 		"""
-		
-		media_item = 0
+		lval_item = self.__motor.getValoracionesItem(idItem)
 		nval = 0
-		for lval_usuario in valoraciones.values():
-			if idItem in lval_usuario.keys():
-				media_item += lval_usuario.get(idItem).valoracion
-				nval += 1
-		media_item = media_item / nval
+		media_item = 0
+		for valoracion in lval_item:
+			if valoracion.idUsu != idUsu: #Obviamos la valoracion del usuario-item a predecir
+				media_item += valoracion.valoracion
+				nval+= 1
+		media_item /= nval
 		return media_item
 		
 		
-	def predice(self, idUsu, idItem, similitudes, valoraciones):
+	def predice(self, idUsu, idItem):
 		"""
 			
 		Metodo que devuelve el valor de prediccion para un item-usuario
 		
 		Params:
-		
-				idItem: Identificador del item cuyo valora deseamos predecir
-				valoraciones(dict): Contiene todos las valoraciones aportadas por todos los usuarios
-				similitudes(dict): Contiene los pares de similitud entre items devueltos por el agrupamiento			
+				idUsu	(Integer):
+				idItem	(Integer): Identificador del item cuyo valora deseamos predecir
 
 		Return:
 					
@@ -79,24 +82,20 @@ class ItemAvgAdj1:
 					
 		"""
 		
-		media_item = self.mediaitem(idItem,valoraciones)
-		media_usuario = self.mediausuario(idUsu,valoraciones)
-		suma_denom = 0
-		suma_num = 0
-		
-		"Cálculo de la fórmula de la prediccion"
-		
-		dsimilares = similitudes.get(idItem)
-		dvaloraciones = valoraciones.get(idUsu)
-		
-		for similitud in dsimilares:
-			idItem2 = similitud.getIdsPels().get(similitud.getIdsPels().index(idItem) - 1)
-			if idItem2 in dvaloraciones.keys():
-				suma_denom += fabs(similitud.similitud)
-				valoracion = dvaloraciones.get(idItem2)
-				suma_num += similitud.similitud * (valoracion.valoracion - media_usuario)
-				
-		vprediccion = suma_num / suma_denom + media_item
+		media_item = self.__mediaitem(idItem)
+		media_usu = self.__mediausuario(idUsu)
+		sum_num = 0
+		sum_den = 0
+		lsim = self.__motor.getSimilitudesItem(idItem)
+		lval = self.__motor.getValoracionesItem(idUsu)
+		#Cálculo de la fórmula de la prediccion		
+		if len(lsim) != len(lval):
+			print 'Error!'
+		for i in len(lsim):
+			if lval[i].idPel != idItem: # Obviamos la casilla del item a predecir
+				sum_num += lsim[i].similitud * (lval[i].valoracion - media_usu)
+				sum_den += fabs(lsim[i].similitud)				
+		vprediccion = sum_num / sum_den + media_item
 		prediccion = Valoracion(idUsu, idItem, vprediccion)
 		return prediccion
 
