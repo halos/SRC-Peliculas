4a373b23f1050402077ef566ca287d5ad0790872
Sergio Ramírez <srg00017@gmail.com>
2011-08-05 13:10:47 +0200

    Añadida más funcionalidad, implementada alguna

--- motor.py
+++ motor.py
@@ -3,13 +3,107 @@
 
 class Motor:
 	""" Class doc """
+	__user = 0
+	__nvaloraciones = 0
 	
 	def __init__ (self):
 		""" Class initialiser """
-		self.__daov = DAOValoracion()
-		self.__daou = DAOUsuario()
-		self.__daop = DAOPelicula()
-		self.__daos = DAOParSimilitud()		
+		pass
+		
+	def login(self, id, passw):
+		""" Function doc
+	
+		Params:
+	
+			PARAM(): DESCRIPTION
+	
+		Return:
+	
+			(): DESCRIPTION
+		"""
+		daou = DAOUsuario()
+		self.__user = daou.getUsuario(id)
+		if self.__comprobarIdentidad(self.__user, passw):
+			self.__actualizarModelo()
+			return True
+		return False
+	
+	def valorarPelicula(self, idPel, val):
+		""" Function doc
+	
+		Params:
+	
+			PARAM(): DESCRIPTION
+	
+		Return:
+	
+			(): DESCRIPTION
+		"""
+		valoracion = Valoracion(self.__user.idUsu, idPel, val)
+		daov = DAOValoracion()
+		daov.inserta(valoracion) # Si existe, se actualiza
+		# Cuando el nº de inserciones sea 5, actualizamos el modelo
+		self.__nvaloraciones += 1
+		if self.__nvaloraciones == 5:
+			self.__actualizarModelo()
+			self.__nvaloraciones = 0
+		
+	def buscarPeliculas(self, consulta):
+		""" Function doc
+	
+		Params:
+	
+			PARAM(): DESCRIPTION
+	
+		Return:
+	
+			(): DESCRIPTION
+		"""
+		daop = DAOPelicula()
+		lpel = daop.getPeliculasTitulo(consulta)
+		return lpel
+	
+	def __comprobarIdentidad(self, user, passw):
+		""" Function doc
+	
+		Params:
+	
+			PARAM(): DESCRIPTION
+	
+		Return:
+	
+			(): DESCRIPTION
+		"""
+		if user.passw == passw:
+			return True
+		return False
+	
+	def __actualizarModelo(self): # Para javi
+		""" Function doc
+	
+		Params:
+	
+			PARAM(): DESCRIPTION
+	
+		Return:
+	
+			(): DESCRIPTION
+		"""
+
+	def recomendar(self):
+			""" Function doc
+	
+		Params:
+	
+			PARAM(): DESCRIPTION
+	
+		Return:
+	
+			(): DESCRIPTION
+		"""
+		# Por implementar (Sergio)
+	
+	# Métodos adicionales (getter's)
 
 	def getValoracionesUsuario(self, idUsu):
 		""" Function doc
@@ -22,7 +116,8 @@ class Motor:
 	
 			(): DESCRIPTION
 		"""
-		return self.__daov.getValoracionesUsuario(idUsu)
+		daov = DAOValoracion()
+		return daov.getValoracionesUsuario(idUsu)
 		
 		
 	def getValoracionesItem(self, idItem):
@@ -36,7 +131,8 @@ class Motor:
 	
 			(): DESCRIPTION
 		"""
-		return self.__daov.getValoracionesItem(idItem)
+		daov = DAOValoracion()
+		return daov.getValoracionesItem(idItem)
 		
 	def getValoraciones(self):
 		""" Function doc
@@ -49,7 +145,8 @@ class Motor:
 	
 			(): DESCRIPTION
 		"""
-		return self.__daov.getValoraciones()
+		daov = DAOValoracion()
+		return daov.getValoraciones()
 		
 	def getSimilitudes(self):
 		""" Function doc
@@ -62,7 +159,8 @@ class Motor:
 	
 			(): DESCRIPTION
 		"""
-		return self.__daos.getSimilitudes()
+		daos = DAOParSimilitud()
+		return daos.getSimilitudes()
 		
 	def getSimilitudesItem(self, idItem):
 		""" Function doc
@@ -75,4 +173,5 @@ class Motor:
 	
 			(): DESCRIPTION
 		"""
-		return self.__daos.getSimilitudesItem(idItem)
+		daos = DAOParSimilitud()
+		return daos.getSimilitudesItem(idItem)
