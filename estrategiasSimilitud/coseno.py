461782d9d56ecc4b9db14b3073df3e64b746a8d2
halos <halos@lambda>
2011-08-08 18:53:44 +0200

    La copia de los parámetros de entrada ahora se hace correctamente

--- estrategiasSimilitud/coseno.py
+++ estrategiasSimilitud/coseno.py
@@ -16,8 +16,8 @@ def calcula_similitud(_p1, _p2):
 		(float): Similitud entre dos películas (0,1)
 	"""
 	
-	p1 = _p1
-	p2 = _p2
+	p1 = dict(_p1)
+	p2 = dict(_p2)
 	
 	# obtener la lista de todos los usuarios que han valorado la película
 	usus = []
