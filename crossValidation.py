0112de06bd8e7a82502bab55f41ea88556ba3c99
Sergio Ramírez <srg00017@gmail.com>
2011-08-18 17:27:09 +0200

    Añadido el método ejecutaIter, para ejecutar iteracion con distinto fold-validation

--- crossValidation.py
+++ crossValidation.py
@@ -18,48 +18,34 @@ class CrossValidation:
 	def __init__(self, k):
 		""" Constructor Básico """
 		self.k = k
-		
-	def ejecutar(self):
 		# Cargamos el fichero de Valoraciones para particionar
-		valoraciones = daov.cargarFicheroPrueba("/home/rebellion/Dropbox/Prácticas/Sistemas Informáticos/ratings1.csv")
+		valoraciones = daoValoracion.DAOValoracion.cargarFicheroPrueba("/home/rebellion/Dropbox/Prácticas/Sistemas Informáticos/ratings1.csv")
 		# Particionamos el espacio en k-folds
-		folds = []
+		self.folds = []
 		sizef = (float) (len(valoraciones) / self.k)
 		for i in range(self.k):
-			folds[i] = []
+			self.folds[i] = []
 			for j in range(sizef):
 				pos = random.randint(0, len(valoraciones) - 1)
-				folds[i].append(valoraciones.pop(pos))
+				self.folds[i].append(valoraciones.pop(pos))
 				if not valoraciones: # está vacía
 					break
+		
+	def ejecutaIter(self, nfold_test):
+		# Utilizar -1 !!
+		if nfold_test > self.k | self.k < 0:
+			print 'Error!'
 		# Borramos el contenido de las tablas similitudes y valoraciones
 		daov = daoValoracion.DAOValoracion()
 		daops = daoParSimilitud.DAOParSimilitud()
 		daov.reset()
 		daops.reset()
+		# Añadimos al espacio (BD) los folds destinados al entrenamiento
+		for i in range(self.k):
+			if i != nfold_test - 1: # Sino es el fold de validación, se añade
+				lval = self.folds[i]
+				for val in lval: # Añadimos cada valoracion del fold
+					daov.inserta(val)
+		# Devolvemos el fold de validacion
+		return self.folds[nfold_test-1]
 		
-		
-	def divTrainTest(self, valoraciones, pct_train):
-		""" Función principal de la clase que divide todas las valoraciones en dos grupos: train y test
-		 
-    		Params:
-    				 
-    			valoraciones(list): Lista con todas las valoraciones de todos los usuarios 
-    			pct_train: Porcentaje en unidades enteras, que indica la proporcion del conjunto train
-    			
-    		Return:
-    		
-    			grupos(tuple): Tupla formada por dos listas de valoraciones (train y test)
-				
-		"""
-		
-		nejtrain = len(valoraciones) * ((float)(pct_train) / 100)
-		copiav = valoraciones[:]
-		train = []
-		test = []
-		# Repartimos instancias de valoraciones entre train, test según los porcentajes
-		for i in range(nejtrain):
-			p = random.randint(0,len(copiav) - 1)
-			train.append(copiav.pop(p))
-		test = copiav[:]
-		return (train,test)
