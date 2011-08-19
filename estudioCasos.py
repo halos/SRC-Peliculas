dc9f3ce33cd17acb21567835366c21ba684309d9
Sergio Ramírez <srg00017@gmail.com>
2011-08-19 11:29:58 +0200

    Terminado. Añadida la cross validation a la prueba

--- estudioCasos.py
+++ estudioCasos.py
@@ -17,40 +17,48 @@ import itemAvgAdjN
 import weithedSum
 import motor
 import db
+import crossValidation
 
-def ejecutaPrueba(p, k, es, ep):
+def ejecutaPrueba(kfold, p, k, es, ep):
     print "Datos:\n"
     print "Particionado:", p + "\%\n"
     print "k:", k + "\n"
     print "Estrategia de similitud:", es[1] , "\n"
     print "Estrategia de prediccion:", ep[1] , "\n"
-    # Comenzamos la medición
-    t_inic = metricas.get_clock()
-    # Regeneramos la base de datos, con un porcentaje de particionamiento
-    dbase = db.DB()
-    valtest = dbase.regenerarDB(p)
-    # Actualizamos el modelo
-    m = motor.Motor()
-    m.actualizarModelo()    
-    # Realizamos el proceso de testing
-    lpredicciones = []
-    for valoracion in valtest:
-        # Calculamos los k-vecinos
-        kval_vec = agrupamiento.Agrupamiento(valoracion.idUsu).agrupknn(valoracion.idPel, k)
-        # Creamos la estrategia de predicción
-        # Predecimos...
-        prediccion = ep.predice(valoracion.idUsu, valoracion.idPel, kval_vec)
-        lpredicciones.append(prediccion)
-    v_mae = metricas.mae(lpredicciones, valtest)
-    
-    t_fin = metricas.get_clock()
-    print "Tiempo: %f\n" % (t_fin - t_inic)    
-    print "Mae:", v_mae, "\n"
-    
+    v_mae = 0
+    time = 0.0
+    # Realizamos el particionamiento
+    crossval = crossValidation.CrossValidation(kfold)
+    # Realizamos k iteraciones y luego realizamos la media aritmética
+    for i in range(kfold):
+        # Comenzamos la medición
+        t_inic = metricas.get_clock()
+        # Particionamos el espacio, siendo el fold 'i' el validador
+        valtest = crossval.ejecutaIter(i)
+        # Actualizamos el modelo ????????????
+        m = motor.Motor()
+        m.actualizarModelo()    
+        # Realizamos el proceso de testing
+        lpredicciones = []
+        for valoracion in valtest:
+            # Calculamos los k-vecinos
+            kval_vec = agrupamiento.Agrupamiento(valoracion.idUsu).agrupknn(valoracion.idPel, k)
+            # Creamos la estrategia de predicción
+            # Predecimos...
+            prediccion = ep.predice(valoracion.idUsu, valoracion.idPel, kval_vec)
+            lpredicciones.append(prediccion)
+        v_mae += metricas.mae(lpredicciones, valtest)
+        # Fin de la medición de tiempo
+        t_fin = metricas.get_clock()
+        time += t_fin - t_inic
+    # Finalmente, realizamos los cálculos de las medias y mostramos
+    print "Tiempo medio: %f\n" % (v_mae / kfold)
+    print "Mae medio:", v_mae, "\n" (time / kfold)
     return None
 
 
 #Definimos los distintos parámetros
+kfold = 5
 tp = {80, 70}
 tk = {3, 5, 10}
 tes = {estrategiaSimilitud.EstrategiaSimilitud(coseno.calcula_similitud), estrategiaSimilitud.EstrategiaSimilitud(pearson.calcula_similitud)}
@@ -61,7 +69,7 @@ print 'Comienzo del estudio de casos para la estrategia de predicción ItemAvgAd
 for p in tp:
     for k in tk:
         for es in tes:
-            ejecutaPrueba(p, k, es, itemAvgAdj1.ItemAvgAdj1())
+            ejecutaPrueba(kfold, p, k, es, itemAvgAdj1.ItemAvgAdj1())
 
 print 'Comienzo del estudio de casos para la estrategia de predicción ItemAvgAdjN:\n'            
  
@@ -70,11 +78,11 @@ for p in tp:
         for es in tes:
             for n in tn:
                 print 'N: ', n , "\n"
-                ejecutaPrueba(p, k, es, itemAvgAdjN.ItemAvgAdjN(n))
+                ejecutaPrueba(kfold, p, k, es, itemAvgAdjN.ItemAvgAdjN(n))
                 
 print 'Comienzo del estudio de casos para la estrategia de predicción WeithedSum:\n'
 
 for p in tp:
     for k in tk:
         for es in tes:
-            ejecutaPrueba(p, k, es, weithedSum.WeithedSum())
+            ejecutaPrueba(kfold, p, k, es, weithedSum.WeithedSum())
